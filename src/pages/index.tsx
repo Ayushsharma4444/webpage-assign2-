167f305525db037b6f4a09e212b527eb629a0f36
Ayushsharma4444 <ayushsharma81852.com>
2023-01-23 16:30:57 +0530

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import {Nav} from './nav';
-import {Header} from './header'; 
-import {Products} from './Products';
-import {Allproducts} from './Allproducts'
+import {Nav} from '../component/nav';
+import {Header} from '../component/header'; 
+import {Products} from '../component/Products';
+import {Allproducts} from '../component/Allproducts'
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
